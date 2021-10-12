80dffeef8b607f6658350e567eb7a01b616c2426
manav shah <shahmanav197@gmail.com>
2021-10-12 11:32:23 +0530

    Project Completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,47 +2,84 @@ import { Container, Grid } from '@material-ui/core';
 import React, { useState } from 'react';
 import logo from "../images/logo.png";
 import { Link } from "react-scroll";
-import { GiHamburgerMenu } from "react-icons/gi"
+import {
+    FaFacebookSquare,
+    FaInstagramSquare,
+    FaYoutubeSquare,
+  } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+// import "../../node_modules/bootstrap/dist/css/bootstrap.css";
+  
 
 const Navbar = () => {
     const [showMediaIcons, setShowMediaIcons] = useState(false);
-
     return (
-        <>
-            <nav className="navbar">
-                <Container>
-                    <Grid container>
-                        <Link className="logo">
-                            <img src={logo} alt="" />
-                        </Link>
+      <>
+        <nav className="main-nav">
+          {/* 1st logo part  */}
+          <div className="logo">
+                <img src={logo} alt="" />
+          </div>
+  
+          {/* 2nd menu part  */}
+          <div
+            className={
+              showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+            }>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">about</Link>
+              </li>
+              <li>
+                <Link to="/service">services</Link>
+              </li>
+              <li>
+                <Link to="/contact">contact</Link>
+              </li>
+            </ul>
+          </div>
+  
+          {/* 3rd social media links */}
+          <div className="social-media">
+            <ul className="social-media-desktop">
+              <li>
+                <a
+                  href="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA"
+                  target="_thapa">
+                  <FaFacebookSquare className="facebook" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/thapatechnical/"
+                  target="_thapa">
+                  <FaInstagramSquare className="instagram" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA"
+                  target="_thapa">
+                  <FaYoutubeSquare className="youtube" />
+                </a>
+              </li>
+            </ul>
+  
+            {/* hamburget menu start  */}
+            <div className="hamburger-menu">
+              <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+                <GiHamburgerMenu />
+              </a>
+            </div>
+          </div>
+        </nav>
+      </>
+    );
+  };
+  
 
-                           
-                            <ul className={
-                            showMediaIcons ? "menu mobile-menu-link" : "menu"
-                                }>
-                                <li><Link to="home">Home</Link></li>
-                                <li><Link to="about">About</Link></li>
-                                <li><Link to="best-sales">Best Sales</Link></li>
-                                <li><Link to="products">Products</Link></li>
-                                <li><Link to="testimonial">Testimonial</Link></li>
-                                <li><Link to="blogs">Blogs</Link></li>
-                                <li><Link to="contact">Contact</Link></li>
-                            </ul>
-                        {/* </div>  */}
-                        <div className="phone-number">
-                            <i class="fas fa-headphones"></i>
-                            <span>(123) 8800-555</span>
-                        </div>
-                        <div className="hamburger-menu">
-                            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
-                                <GiHamburgerMenu />
-                            </a>
-                        </div>
-                    </Grid>
-                </Container>
-            </nav>   
-        </>
-    )
-}
 
 export default Navbar;
